adc56e42e7bef84158355309b5ead9291ed7a26e
kushal-png <kushalbnl2002@gmail.com>
2024-06-20 02:32:27 +0530

    search functionality

--- app/views.py
+++ app/views.py
@@ -25,31 +25,34 @@ def recipe(request):
 
 def show(request):
     recipeList = models.recipeModel.objects.all()
+    if request.GET.get("search"):
+        recipeList = recipeList.filter(
+            title__icontains=request.GET.get("search"))
     context = {"recipes": recipeList}
     return render(request, 'show.html', context)
 
+
 def destroy(request, id):
-    recipe= models.recipeModel.objects.get(id=id)
+    recipe = models.recipeModel.objects.get(id=id)
     recipe.delete()
     return redirect("/show")
 
+
 def update(request, id):
-    recipe= models.recipeModel.objects.get(id=id)
+    recipe = models.recipeModel.objects.get(id=id)
     if request.method == "POST":
         data = request.POST
         rtitle = data.get('ftitle')
         rdescription = data.get('fdescription')
         rimage = request.FILES.get('fimage')
 
-        recipe.title=rtitle
-        recipe.description=rdescription
+        recipe.title = rtitle
+        recipe.description = rdescription
         if rimage:
-            recipe.image=rimage
-        
+            recipe.image = rimage
+
         recipe.save()
         return redirect("/show")
-    
-    context={'recipes':recipe}
+
+    context = {'recipes': recipe}
     return render(request, 'update.html', context)
-    
-    
